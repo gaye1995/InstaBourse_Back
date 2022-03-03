d747ed0d9bacf4c3af74aab380c7629e7c882709
Gaye Mboup <gayemboup@MacBook-Pro-de-Gaye.local>
2022-03-03 15:01:34 +0100

    controller user

--- src/controllers/UserControllers/UserController.ts
+++ src/controllers/UserControllers/UserController.ts
@@ -28,37 +28,4 @@ export class UserController {
 
     } 
 
-// static login = async (req: Request, res: Response) => {
-//     try {
-//         const { email, password } = req.body;
-//         if (!email || !password) throw { code: 400 };
-//         let user: any = await User.findOne({email : email});
-//         // email doesn't existe 
-//         if (!user) throw { code: 402 }
-
-//         // verifier les tentatives de connexion
-//         // const lastLogin = (Date.now() - User.lastLogin) / 1000;
-
-//         // Si l'utilisateur à respecter les deux minutes d'attente on remet sont nombres d'essai à 0
-//         // if (User.attempt >= 5 && lastLogin > 300) await ComptableModel.updateOne({ _id: mongoose.Types.ObjectId(User._id)} ,{  $set: { lastLogin: Date.now(), attempt: User.attempt + 1 } });
-
-//         // On vérifie le nombre de connnexion et le temps depuis la dernière connexion
-//         // if (User.attempt >= 5 && lastLogin < 300) throw new Error('Too many attempts on this email (5 max) - Please wait (5min)');
-
-//         if(!await comparePassword(password, User.password)){
-//             throw { code: 404}
-//         } 
-
-//         User = await jwt.getAuthToken(User);
-//         const dataUser: any = UserJSON(User);
-//         await updateLastLogin(User, true);
-//         // Envoi de la réponse
-//         res.status(200).send({ error: false, message: 'The user has been successfully connected', user: User });
-//     } catch (err) {
-//         if (err.code === 400) res.status(400).send({ error: true, message: 'One or more mandatory data is missing' });
-//         else if (err.code === 402) res.status(400).send({ error: true, message: 'An account using this email address does not exist' });
-//         else if (err.code === 404) res.status(409).send({ error: true, message: 'One of your data is incorrect' });
-//         else Datahelpers.errorHandler(res, err);
-//     }
-//     }
 }
